88e28b4fd504aa85ea9209b572efbe852796f5a7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-29 11:02:22 -0400

    upgrade cypress, add completed todo test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -19,3 +19,19 @@ it('works', () => {
 
   cy.screenshot('finished', { capture: 'runner' })
 })
+
+it('completes second item', () => {
+  cy.visit('/')
+  // application starts with 3 todos
+  cy.get('.todo')
+    .should('have.length', 3)
+    .eq(1)
+    .contains('[data-cy=complete]', 'Complete')
+    .wait(1000, { log: false }) // for better video
+    .click()
+
+  cy.get('.todo').eq(1).should('have.class', 'completed')
+  // the other items are not completed
+  cy.get('.todo').eq(0).should('not.have.class', 'completed')
+  cy.get('.todo').eq(2).should('not.have.class', 'completed')
+})
