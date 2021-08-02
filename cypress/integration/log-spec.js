0d6c48c3f8f5e8df52a5ae5c4aceafb594278c48
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-08-02 11:10:28 -0400

    Visit blank page before the tests (#6)
    * add log spec
    
    * move visit blank page to the support file

---
+++ cypress/integration/log-spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+beforeEach(() => {
+  cy.visit('/', {
+    onBeforeLoad(win) {
+      cy.spy(win.console, 'log').as('log')
+    },
+  })
+  // the app has loaded
+  cy.get('.todo').should('have.length', 3)
+})
+
+it('logs message on startup', () => {
+  cy.get('@log').should('have.been.calledOnceWithExactly', 'rendering app')
+})
+
+it('logs message when adding a todo', () => {
+  cy.get('@log').invoke('resetHistory') // reset the spy
+  cy.get('[data-cy=new-todo]').type('hello{enter}')
+  cy.get('@log').should('have.been.calledOnceWithExactly', 'added todo')
+})
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,3 +3,12 @@
 // load and register the grep feature
 // https://github.com/bahmutov/cypress-grep
 require('cypress-grep')()
+
+function visitBlankPage() {
+  cy.window().then((win) => {
+    win.location.href = 'about:blank'
+  })
+}
+
+// read https://glebbahmutov.com/blog/visit-blank-page-between-tests/
+beforeEach(visitBlankPage)
