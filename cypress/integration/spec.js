a2c9e5e6f81ed778f92051087317ae9c3f7dfafe
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-29 12:00:29 -0400

    add second spec

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -42,19 +42,3 @@ it('works', () => {
 it('has no visit logic', () => {
   cy.wrap(42).wait(1000).should('equal', 42)
 })
-
-it('completes second item', () => {
-  cy.visit('/')
-  // application starts with 3 todos
-  cy.get('.todo')
-    .should('have.length', 3)
-    .eq(1)
-    .contains('[data-cy=complete]', 'Complete')
-    .wait(1000, { log: false }) // for better video
-    .click()
-
-  cy.get('.todo').eq(1).should('have.class', 'completed')
-  // the other items are not completed
-  cy.get('.todo').eq(0).should('not.have.class', 'completed')
-  cy.get('.todo').eq(2).should('not.have.class', 'completed')
-})
